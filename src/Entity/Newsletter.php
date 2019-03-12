3f3e369048247f11722e091e3e47e162a4633bb3
Thomas75019 <tlarousse3@gmail.com>
2019-03-12 02:47:18 +0100

    change Newsletter entity

--- src/Entity/Newsletter.php
+++ src/Entity/Newsletter.php
@@ -21,19 +21,6 @@ class Newsletter
      */
     private $id;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="first_name", type="string", length=255, nullable=false)
-     */
-    private $firstName;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="last_name", type="string", length=255, nullable=false)
-     */
-    private $lastName;
 
     /**
      * @var string
@@ -45,30 +32,6 @@ class Newsletter
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getFirstName(): ?string
-    {
-        return $this->firstName;
-    }
-
-    public function setFirstName(string $firstName): self
-    {
-        $this->firstName = $firstName;
-
-        return $this;
-    }
-
-    public function getLastName(): ?string
-    {
-        return $this->lastName;
-    }
-
-    public function setLastName(string $lastName): self
-    {
-        $this->lastName = $lastName;
-
-        return $this;
     }
 
     public function getEmail(): ?string
