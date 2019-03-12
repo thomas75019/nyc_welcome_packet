81e6df0d02ae531a1460c85b6a4c701b885666c7
Thomas75019 <tlarousse3@gmail.com>
2019-03-12 22:57:47 +0100

    Newsletter mehtod

---
+++ src/Controller/NewsletterController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Newsletter;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+Class NewsletterController extends AbstractController
+{
+    /**
+     * @Route("/newsletter" name="newsletter_form" methods={"GET", "POST"})
+     */
+    public function newsletter(Request $request) : Response
+    {
+        $newsletter = new Newsletter();
+        $form = $this->createForm(NewsletterType::class, $newsletter);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($newsletter);
+            $em->flush();
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('home/index.html.twig');
+
+    }
+}
